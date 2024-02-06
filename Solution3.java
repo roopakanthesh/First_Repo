b563c9e9cd7db671a3595aec1e1916816a564800
roopakanthesh <143074436+roopakanthesh@users.noreply.github.com>
2024-02-06 11:37:55 +0530

    Add files via upload

---
+++ Solution3.java
@@ -0,0 +1,10 @@
+package com.kodnest.accessmodifiers2;
+import com.kodnest.accessmodifiers1.*;
+
+
+class Solution3 extends Solution1 {
+	
+    void display() {
+    	System.out.println(new Solution3().a);
+    }
+}
---
+++ Solution4.java
@@ -0,0 +1,9 @@
+package com.kodnest.accessmodifiers2;
+
+class Solution4 {
+	
+	void display() {
+		System.out.println(new Solution3().a);
+	}
+
+}
