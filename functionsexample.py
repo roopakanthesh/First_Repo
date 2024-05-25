443ee146b467d135c0dececd969690bc31bd2334
roopakanthesh <143074436+roopakanthesh@users.noreply.github.com>
2024-05-25 19:09:41 +0530

    Add files via upload

---
+++ functionsexample.py
@@ -0,0 +1,34 @@
+def add():
+    print("Addition")
+    a=int(input("enter number1:"))
+    b=int(input("enter number2:"))
+    print(a+b)
+add()
+
+def sub():
+    print("substraction")
+    a=int(input("enter number1:"))
+    b=int(input("enter number2:"))
+    print(a-b)
+sub()
+
+def prod():
+    print("Multiplication")
+    a=int(input("enter number1:"))
+    b=int(input("enter number2:"))
+    print(a*b)
+prod()
+
+def div():
+    print("Division")
+    a=int(input("enter number1:"))
+    b=int(input("enter number2:"))
+    print(a/b)
+div()
+
+def rem():
+    print("Remainder")
+    a=int(input("enter number1:"))
+    b=int(input("enter number2:"))
+    print(a%b)
+rem()
