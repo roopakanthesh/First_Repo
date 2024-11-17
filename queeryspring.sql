7f9d1601437a72a56f63cd3a7f2f94cf834ec027
roopakanthesh <143074436+roopakanthesh@users.noreply.github.com>
2024-11-17 09:50:50 -0300

    Add files via upload

---
+++ queeryspring.sql
@@ -0,0 +1,7 @@
+create database kodclass;
+use kodclass;
+create table laptop(id int primary key,name varchar(20),color varchar(20));
+
+
+select * from laptop;
+insert into laptop values(1,'lenevo','silver'),(2,'hp','white');
