d7e1a8a9b2a7ab3030c02b8e40c79541751e9dba
roopakanthesh <143074436+roopakanthesh@users.noreply.github.com>
2024-12-31 12:14:33 -0300

    Add files via upload

--- Solution2.java
+++ Solution2.java
@@ -1,13 +1,8 @@
-package demo.kodnest.practisepatterns;
+package com.kodnest.accessmodifiers1;
 
-public class solution2 {
-	public static void main(String[] args) {
-		for(int i=1;i<=6;i++) {
-			for(int j=1;j<=i;j++) {
-				System.out.print("*");
-			}
-		System.out.println();
-		}
-	}
-
+class Solution2 {
+  
+   void display() {
+	   System.out.println(new Solution1().a);
+   }
 }
