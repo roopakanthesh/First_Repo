551a3a211a78a2f195c3cc42ce6b64ddd08ebaf5
roopakanthesh <143074436+roopakanthesh@users.noreply.github.com>
2025-01-08 13:06:25 -0300

    Add files via upload

---
+++ Add.js
@@ -0,0 +1,30 @@
+//let a=10;
+//console.log(a);
+
+export function add(a,b){
+    console.log("sum = "+ (a+b));
+}
+
+export function sub(a,b){
+    console.log("Difference = "+ (a-b));
+}
+
+export function mul(a,b){
+    console.log("Producct = "+(a*b));
+}
+
+export function div(a,b){
+    console.log("Quotient = "+(a/b));
+}
+
+export function mod(a,b){
+    console.log("Remainder = "+(a%b));
+}
+export const Pi = 3.14;
+export class Student{
+    id=10;
+    study(){
+        console.log("studying");
+    }
+}
+
