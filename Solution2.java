f9a3968ffd11e0f1dd897fcbc9b1435616903da9
roopakanthesh <143074436+roopakanthesh@users.noreply.github.com>
2025-03-12 07:57:29 -0300

    Add files via upload

--- Solution2.java
+++ Solution2.java
@@ -1,8 +1,15 @@
-package com.kodnest.accessmodifiers1;
+package com.kodnest.exception;
 
-class Solution2 {
-  
-   void display() {
-	   System.out.println(new Solution1().a);
+public class Solution2 {
+	public static void main(String[] args) {
+		method1();
+	}
+	
+   static void method1() {
+	   method2();
+   }
+   
+   static void method2(){
+	   System.out.println(3/0);
    }
 }
